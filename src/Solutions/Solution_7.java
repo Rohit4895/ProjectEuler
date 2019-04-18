5d85764f372f696752a9e8733f884bad9c6316a8
Rohit <rohit.ramtirth@gmail.com>
2019-04-18 17:08:48 +0530

    Problem 7 solved

---
+++ src/Solutions/Solution_7.java
@@ -0,0 +1,38 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_7 {
+	public static void main(String args[]) {
+		
+		/*
+		 * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+		 * that the 6th prime is 13.
+		 * 
+		 * What is the 10 001st prime number?
+		 */
+		
+		int count = 1, n=3, result=0;
+		boolean prime = true;
+		
+		while (count < 10001) {
+			
+			for(int i=1; i<=(n/2); i++) {
+				if (i!=1) {
+					if(n%i == 0)
+						prime=false;
+				}
+			}
+			if(prime) {
+				count++;
+				result=n;
+			}
+			prime=true;
+			n++;
+		}
+		
+		System.out.println("10001 st prime number is: "+result);
+	}
+
+}
