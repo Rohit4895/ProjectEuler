f78bd45b62e66cbdc9610468c1116293c2b3cd9d
Rohit <rohit.ramtirth@gmail.com>
2019-04-23 17:46:59 +0530

    Problem 9 solved

---
+++ src/Solutions/Solution_9.java
@@ -0,0 +1,24 @@
+package Solutions;
+
+public class Solution_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long c=0, round=0;
+		for(int i=50; i<=500; i++) {
+			for(int j=50; j<=500; j++) {
+				if(i<j) {
+					c = (i*i)+(j*j);
+					double prev = Math.sqrt(c);
+					double next = Math.floor(prev);
+					if(prev == next) {
+						if((i+j+prev)==1000)
+							System.out.println((int)(i*j*prev));
+					}
+				}
+			}
+		}
+
+	}
+
+}
